091cba187e2e030d1f0e5c85896c763d47437f65
Swagh001 <shaileshwagh300@gmail.com>
2023-04-12 19:26:33 +0530

    Address files added with some code(html,css,js)

---
+++ Address/address.js
@@ -0,0 +1,74 @@
+
+document.querySelector("form").addEventListener("submit",submit);
+let AddressArr= JSON.parse(localStorage.getItem("AddressData")) || [];
+function submit(){
+    event.preventDefault();
+    let name=document.getElementById("name").value;
+    let mobile=document.getElementById("mobile").value;
+    let address=document.getElementById("address").value;
+    let landmark=document.getElementById("landmark").value;
+    let pincode=document.getElementById("pincode").value;
+
+    console.log("hello"+name);
+
+    addressObj={
+        name:name,
+        mobile:mobile,
+        address:address,
+        landmark:landmark,
+        pincode:pincode,
+    };
+
+    AddressArr.push(addressObj);
+    
+    localStorage.setItem("AddressData",JSON.stringify(AddressArr));
+}
+let home=document.querySelector(".btn-1");
+let office=document.querySelector(".btn-2");
+let other=document.querySelector(".btn-3");
+
+if(home.addEventListener("click",addressSelect)){}
+else if(office.addEventListener("click",addressSelect2)){}
+else if(other.addEventListener("click",addressSelect3)){}
+
+
+function addressSelect(){
+    var selectedBtn2=document.querySelector(".btn-2");
+    selectedBtn2.style.backgroundColor="white";
+    var selectedBtn3=document.querySelector(".btn-3");
+    selectedBtn3.style.backgroundColor="white";
+
+    var selectedBtn=document.querySelector(".btn-1");
+    selectedBtn.style.backgroundColor="rgb(216, 214, 214)";
+    console.log(selectedBtn.value);
+}
+
+function addressSelect2(){
+    var selectedBtn=document.querySelector(".btn-1");
+    selectedBtn.style.backgroundColor="white";
+    var selectedBtn3=document.querySelector(".btn-3");
+    selectedBtn3.style.backgroundColor="white";
+
+    var selectedBtn2=document.querySelector(".btn-2");
+    selectedBtn2.style.backgroundColor="rgb(216, 214, 214)";
+    console.log(selectedBtn2.value);
+}
+
+function addressSelect3(){
+    var selectedBtn=document.querySelector(".btn-1");
+    selectedBtn.style.backgroundColor="white";
+    var selectedBtn2=document.querySelector(".btn-2");
+    selectedBtn2.style.backgroundColor="white";
+
+    var selectedBtn3=document.querySelector(".btn-3");
+    selectedBtn3.style.backgroundColor="rgb(216, 214, 214)";
+    console.log(selectedBtn3.value);
+
+    let new_div=document.createElement("div");
+    new_div.setAttribute("id","newinput")
+    let new_input=document.createElement("input");
+    new_input.setAttribute("id","new_input_1");
+    new_input.placeholder="Eg: Friend's Place";
+    new_div.append(new_input);
+    document.getElementById("newInut-1").append(new_div)
+}
